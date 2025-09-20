e902f3e9970ca701eaf3db409f6503648b7fb36b
Amit Upadhyay <2648amitupadhyay@gmail.com>
2025-09-20 12:59:06 +0530

    Add keyboard

--- App.jsx
+++ App.jsx
@@ -6,14 +6,24 @@ import Languages from "./components/Languages"
 
 export default function AssemblyEndgame() {
   const [currentWord, setCurrentWord] = useState("react")
-  const array = currentWord.split("")
 
-  const letterChips = array.map((letter, index) => {
+  const array = currentWord.split("")
+  const alphabets = "abcdefghijklmnopqrstuvwxyz".split("")
+
+  const wordRevealerChips = array.map((letter, index) => {
     const styles = {
       color: "#F9F4DA",
     }
 
     return <span key={index} style={styles}>{letter.toUpperCase()}</span>
+  })
+
+  const keyboardChips = alphabets.map((letter, index) => {
+    const styles = {
+      backgroundColor: "#FCBA29",
+    }
+
+    return <button key={letter} style={styles}>{letter.toUpperCase()}</button>
   })
 
   return (
@@ -22,9 +32,14 @@ export default function AssemblyEndgame() {
         <Header />
         <StatusBar />
         <Languages />
+
         <div className="word-revealer">
-          {letterChips}
+          {wordRevealerChips}
         </div>
+      </div>
+
+      <div className="keyboard">
+        {keyboardChips}
       </div>
     </main>
   )
