fd0a13417d61292edf214c7c2fd0504ad86cbbfe
Amit Upadhyay <2648amitupadhyay@gmail.com>
2025-09-20 12:15:37 +0530

    Add the word revealer

--- App.jsx
+++ App.jsx
@@ -1,14 +1,20 @@
-import React from "react"
+import { useState } from "react"
 
 import Header from "./components/Header"
 import StatusBar from "./components/StatusBar"
 import Languages from "./components/Languages"
 
 export default function AssemblyEndgame() {
-  const [currentWord, setCurrentWord] = React.useState("react")
+  const [currentWord, setCurrentWord] = useState("react")
   const array = currentWord.split("")
 
-  
+  const letterChips = array.map((letter, index) => {
+    const styles = {
+      color: "#F9F4DA",
+    }
+
+    return <span key={index} style={styles}>{letter.toUpperCase()}</span>
+  })
 
   return (
     <main>
@@ -16,6 +22,9 @@ export default function AssemblyEndgame() {
         <Header />
         <StatusBar />
         <Languages />
+        <div className="word-revealer">
+          {letterChips}
+        </div>
       </div>
     </main>
   )
